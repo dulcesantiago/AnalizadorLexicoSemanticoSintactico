c146f25255265211e3b55ac44371fe1e2bfd72e8
dulcesantiago <L18730278@pinotepa.tecnm.mx>
2021-10-26 20:56:34 -0500

    Proyecto en lenguaje de programacion Java Analizador

---
+++ src/codigo/Principal.java
@@ -0,0 +1,43 @@
+
+package codigo;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        String ruta1 = "C:/Analizador/AnalizadorLexico_Semantico_Sintactico/src/codigo/Lexer.flex";
+        String ruta2 = "C:/Analizador/AnalizadorLexico_Semantico_Sintactico/src/codigo/LexerCup.flex";
+        String[] rutaS = {"-parser", "Sintax", "C:/Analizador/AnalizadorLexico_Semantico_Sintactico/src/codigo/Sintax.cup"};
+        generar(ruta1, ruta2, rutaS);
+    }
+    public static void generar(String ruta1, String ruta2, String[] rutaS) throws IOException, Exception{
+        File archivo;
+        archivo = new File(ruta1);
+        JFlex.Main.generate(archivo);
+        archivo = new File(ruta2);
+        JFlex.Main.generate(archivo);
+        java_cup.Main.main(rutaS);
+        
+        Path rutaSym = Paths.get("C:/Analizador/AnalizadorLexico_Semantico_Sintactico/src/codigo/sym.java");
+        if (Files.exists(rutaSym)) {
+            Files.delete(rutaSym);
+        }
+        Files.move(
+                Paths.get("C:/Analizador/AnalizadorLexico_Semantico_Sintactico/sym.java"), 
+                Paths.get("C:/Analizador/AnalizadorLexico_Semantico_Sintactico/src/codigo/sym.java")
+        );
+        Path rutaSin = Paths.get("C:/Analizador/AnalizadorLexico_Semantico_Sintactico/src/codigo/Sintax.java");
+        if (Files.exists(rutaSin)) {
+            Files.delete(rutaSin);
+        }
+        Files.move(
+                Paths.get("C:/Analizador/AnalizadorLexico_Semantico_Sintactico/Sintax.java"), 
+                Paths.get("C:/Analizador/AnalizadorLexico_Semantico_Sintactico/src/codigo/Sintax.java")
+        );
+    }
+}
